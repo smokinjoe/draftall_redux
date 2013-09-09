689ca0206e51083411d5a297dc6bd715f8cf32fc
Smokin Joe ChOS <joe.ekiert@gmail.com>
2013-09-09 08:19:35 +0000

    adding validation for presence of title, as well as ability to create new game recors

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,19 @@ class GamesController < ApplicationController
   def new
     @game = Game.new
   end
+  
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to @game
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  def game_params
+    params.require(:game).permit(:title)
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
+  validates :title, presence: true
   has_many :players
 end
