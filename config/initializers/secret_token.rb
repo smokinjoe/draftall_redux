4c8c27d9ae5a920fbcf3b79d7133419bd397bfff
Smokin Joe ChOS <joe.ekiert@gmail.com>
2013-09-09 04:15:42 +0000

    initial commit - layout stuffs are started and in good shape

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Draftall::Application.config.secret_key_base = 'aa8839af7a7d335e3726807251ef50d6374d886d1550240aa5756eeb862a3bd661f2a02af306674cb02aa30bb53f9de80e51ad81832c9df412fd69d2bfad3c59'
